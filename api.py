104e22550163f53188a0e9348d952d6822d71bac
Katy Barone <katherine.barone10@gmail.com>
2022-03-01 10:09:53 -0600

    adding code to get api calls into dataframes

--- api.py
+++ api.py
@@ -3,6 +3,9 @@ Trying out the API stuff
 '''
 
 import requests
+import io
+import pandas as pd
+
 
 
 zillow = "https://api.bridgedataoutput.com/api/v2/zgecon/cut?access_token=91d62741f0adc2b89799df25bcc9299e&regionTypeID=1&metricTypeKey=ZRI"
@@ -33,3 +36,15 @@ mashvisor6 = "https://api.mashvisor.com/v1.1/client/neighborhood/7877/historical
 census1 = "https://api.census.gov/data/2019/acs/acs1?get=NAME,B01001_001E&for=county:*&in=state:*&key=2a3bf1bd5b110158358335717d5d067b0e377810"
 
 census2 = "https://api.census.gov/data/timeseries/poverty/saipe?get=SAEMHI_PT,NAME&for=county:*&in=state:*&time=2020&key=2a3bf1bd5b110158358335717d5d067b0e377810"
+
+rawData = pd.read_csv(io.StringIO(response.decode('utf-8')))
+rawData = rawData.rename(columns= {'[["SAEMHI_PT"':"med_inc","NAME":"county","county]":"fips_county"})
+df = rawData.copy()
+df = df.iloc[:,:5]
+df['med_inc'] = df['med_inc'].str.replace("[","")
+
+cols_to_fix = ["med_inc", "fips_county"]
+for col in cols_to_fix:
+    df[col] = df[col].str.replace("[","")
+    df[col] = df[col].str.replace("]","")
+    df[col] = df[col].str.replace('"',"")
