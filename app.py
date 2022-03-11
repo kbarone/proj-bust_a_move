cdfae2ccfd5703846e7b5fa786a682568c4bae2f
Norah Carney Griffin <ncg@linux6.cs.uchicago.edu>
2022-03-10 22:50:09 -0600

    Formatting updates!

--- app.py
+++ app.py
@@ -10,6 +10,11 @@ from dash.dependencies import Input, Output
 
 app = Dash(__name__)
 
+colors = {
+    'background': '#FDFEFE',
+    'text': '#17202A'
+}
+
 styles = {
     'pre': {
         'border': 'thin lightgrey solid',
@@ -27,9 +32,15 @@ natl_parks = pd.read_csv("natl_parks.csv")
 fig = go.Figure(go.Choroplethmapbox(geojson=counties, locations=zhvi_county_inc_pop["FIPS"], z=zhvi_county_inc_pop["2021_2yr_increase"],
                                     colorscale="Jet", zmin=-17, zmax=58, text=zhvi_county_inc_pop["text_2yrs"], 
                                     marker_opacity=0.5, marker_line_width=0))
-fig.update_layout(mapbox_style="carto-positron",
+fig.update_layout(mapbox_style="carto-positron", # style options: "basic", "streets", "outdoors", 
+                # "dark", "satellite", or "satellite-streets","light"
+                # "open-street-map", "carto-positron", 
+                # "carto-darkmatter", "stamen-terrain", 
+                # "stamen-toner" or "stamen-watercolor"
                   mapbox_zoom=3, mapbox_center = {"lat": 37.0902, "lon": -95.7129})
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig.update_layout(
+    title="Changes in Housing Prices from 2019 to 2021")
 fig.add_trace(go.Scattermapbox(
     lat= natl_parks['latitude'],
     lon= natl_parks['longitude'],
@@ -38,17 +49,42 @@ fig.add_trace(go.Scattermapbox(
         size=6,
         color = 'black'),
     text = natl_parks["park_name"], hoverinfo = 'skip'))
+fig.update_layout(title={
+        'text': "Percent Change in Housing Prices from 2019 to 2021",
+        'y': 0.96,
+        'x':0.4,
+        'xanchor': 'center',
+        'yanchor': 'top'})
 fig.update_layout(clickmode='event+select')
 
 
 
 
-app.layout = html.Div([
+app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+
+    html.H1(
+        children='Bust A Move!',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
+
+    html.Div(children='Exploring Regional Mobility in the US\n', style={
+        'textAlign': 'center',
+        'color': colors['text']
+    }),
+
+
+    html.Div(children='Click on a county on the map to view mobility and demographic data.', style={
+        'textAlign': 'center',
+        'color': colors['text']
+    }),
 
     html.Div([
         dcc.Graph(
             id='zhvf',
-            clickData={'points': [{'location': 'Japan'}]},
+            clickData={'points': [{'location': '30029'}]},
             figure=fig
     )], style={'width': '60%','display': 'inline-block'}),
 
@@ -81,30 +117,30 @@ def update_time_series(clickData):
     fig2.add_trace(go.Scatter(
         x= mobility[mobility["countyfips"] == county]["date"],
         y= mobility[mobility["countyfips"] == county]["gps_parks"],
-        name = 'parks', 
+        name = 'Parks', 
         connectgaps=True
     ))
     fig2.add_trace(go.Scatter(
         x= mobility[mobility["countyfips"] == county]["date"],
         y= mobility[mobility["countyfips"] == county]["gps_retail_and_recreation"],
-        name = 'retail and recreation', 
+        name = 'Retail and Recreation', 
         connectgaps=True
     ))
     fig2.add_trace(go.Scatter(
         x= mobility[mobility["countyfips"] == county]["date"],
         y= mobility[mobility["countyfips"] == county]["gps_grocery_and_pharmacy"],
-        name = 'grocery and pharmacy',
+        name = 'Grocery and Pharmacy',
         connectgaps=True
     ))
     fig2.add_trace(go.Scatter(
         x= mobility[mobility["countyfips"] == county]["date"],
         y= mobility[mobility["countyfips"] == county]["gps_residential"],
-        name = 'residential', 
+        name = 'Residential', 
         connectgaps=True
     ))
     fig2.update_layout(legend=dict(
-        yanchor="top",
-        y=1.5,
+        yanchor="bottom",
+        y= -0.75,
         xanchor="left",
         x=0.01
     ))
@@ -112,9 +148,9 @@ def update_time_series(clickData):
                        xref='paper', yref='paper', showarrow=False, align='left',
                        text=county)
 
+    fig2.update_layout(title="Percent change in GPS activity by Category")
+
     return fig2
-
-
 
 
 if __name__ == '__main__':
