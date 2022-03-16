b0f679c3bded7d5d2e882d933751f255e0b251cc
Katy Barone <katherine.barone10@gmail.com>
2022-03-16 13:39:31 -0500

    adding in new requirements and install.sh

--- install.sh
+++ install.sh
@@ -8,14 +8,6 @@ echo -e "2. Installing Requirements..."
 
 source env/bin/activate
 pip install -r requirements.txt
-'''
-echo -e "3. Installing Selenium Drivers..."
-if [[ $PLATFORM == 'Linux' ]];  then 
-    wget https://github.com/mozilla/geckodriver/releases/download/v0.30.0/geckodriver-v0.30.0-linux64.tar.gz
-    tar -xvf geckodriver-v0.30.0-linux64.tar.gz
-    rm -rf geckodriver-v0.30.0-linux64.tar.gz 
-    mv geckodriver env/bin/ 
-fi
-'''
+
 deactivate 
 echo -e "Install is complete."
